b6c120f04f5b68b20924946c306bce5ecc734094
Isabel Sáenz <isasaete@gmail.comn>
2025-03-05 18:57:09 +0100

    Add function to show total number of paragraphs

--- src/ui/analysis.ts
+++ src/ui/analysis.ts
@@ -1,3 +1,5 @@
+import { getParagraphsTotalNumber } from "../analysis/index.js";
+
 const totalsContainer = document.querySelector(".totals");
 const shortWordsContainer = document.querySelector(".short");
 const palindromesContainer = document.querySelector(".palindromes");
@@ -119,6 +121,7 @@ export const analyzeText = (text: string): void => {
   // Keep this two calls
   listenFrequenciesChanges(text);
   listenForbiddenWordsChanges(text);
+  renderParagraphsTotal(getParagraphsTotalNumber(text));
 
   // Implement the rest of the analysis here
 };
